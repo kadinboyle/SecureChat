627651b233c613326bbec10b014de1e4a33f1901
Kadin Boyle <boyka182@student.otago.ac.nz>
2015-06-27 14:39:22 +1200

    Fix checkout

--- Client/Program.cs
+++ Client/Program.cs
@@ -14,6 +14,7 @@ namespace ClientProgram {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ClientMain());
+            //test
         }
     }
 }
